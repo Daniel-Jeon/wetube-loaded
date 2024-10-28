890adadf2b7710f3c1ae9475c69328f2e1a4cc64
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2024-10-28 16:08:30 +0900

    why can't fix error?
    what the fuck..?

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,11 +1,9 @@
 // Home
-export const trending = (req, res) => res.send("Homepage Videos");
+export const trending = (req, res) => res.render("see");
+
+export const see = (req, res) => res.render("home");
 
 export const search = (req, res) => res.send("Search");
-export const see = (req, res) => {
-  console.log(req.params);
-  return res.send(`Watch Video #${req.params.id}`);
-};
 export const upload = (req, res) => res.send("Upload");
 export const edit = (req, res) => res.send("Edit");
 export const videoDelete = (req, res) => res.send("Delete");
--- src/server.js
+++ src/server.js
@@ -7,8 +7,12 @@ import userRouter from "./routers/userRouter";
 const PORT = 4000;
 
 const app = express();
-app.use(morgan("dev"));
+const logger = morgan("dev");
 
+app.set("view engine", "pug");
+app.set("views", process.cwd() + "/src/views");
+
+app.use(logger);
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
