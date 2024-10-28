a22a4e0d2586e33d50d2d6dcdc66ece95e25d8a1
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2024-10-28 22:46:23 +0900

    Study Pug :dog:
    Pug
    partial
    inheritance
    block
    make variable in pug

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,9 +1,6 @@
-// Home
-export const trending = (req, res) => res.render("home");
-
+export const trending = (req, res) => res.render("home", { pageTitle: "home" });
 export const see = (req, res) => res.render("watch");
-
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
-export const edit = (req, res) => res.send("Edit");
-export const videoDelete = (req, res) => res.send("Delete");
+export const deleteVideo = (req, res) => res.send("Delete");
