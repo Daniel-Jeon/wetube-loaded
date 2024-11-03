ca3ab1fc1cb6ffa271ff6fdb4df57ce2050b57bd
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2024-11-03 23:14:52 +0900

    created first data
    but not yet in db :(

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,4 +1,4 @@
-import Video from "../models/Video";
+import Video from "../models/Video.js";
 
 export const home = async (req, res) => {
   try {
@@ -27,6 +27,17 @@ export const getUpload = (req, res) => {
 };
 
 export const postUpload = (req, res) => {
-  const { title } = req.body;
+  const { title, description, hashtags } = req.body;
+  const video = new Video({
+    title,
+    description,
+    createdAt: Date.now(),
+    hashtags: hashtags.split(",").map((word) => `#${word}`),
+    meta: {
+      views: 0,
+      rating: 0,
+    },
+  });
+  console.log(video);
   return res.redirect("/");
 };
