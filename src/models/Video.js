42cc81f3854e3b055ecd1e7ef2c2d609e8139cf2
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2024-11-13 23:58:57 +0900

    upload video wow !
    very nice

--- src/models/Video.js
+++ src/models/Video.js
@@ -4,6 +4,7 @@ const videoSchema = new mongoose.Schema({
   /* 수동적으로 할수도 있던 것들을 mongoose덕에 자동으로 할 수 있음
      데이터에 대한 구체적인 설정을 하는게 중요함 */
   title: { type: String, required: true, trim: true },
+  fileUrl: { type: String, required: true },
   description: { type: String, required: true, trim: true },
   createdAt: { type: Date, required: true, default: Date.now() },
   hashtags: [{ type: String, trim: true }],
