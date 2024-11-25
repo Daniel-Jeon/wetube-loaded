760b72dcbdef49e630ce1375698e1d71242fab85
Daniel Jeon <45351514+Daniel-Jeon@users.noreply.github.com>
2024-11-25 22:57:41 +0900

    comment db save success
    but not realtime

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,5 +1,6 @@
 import User from "../models/User";
 import Video from "../models/Video";
+import Comment from "../models/Comment";
 
 export const home = async (req, res) => {
   const videos = await Video.find({})
@@ -9,7 +10,8 @@ export const home = async (req, res) => {
 };
 export const watch = async (req, res) => {
   const { id } = req.params;
-  const video = await Video.findById(id).populate("owner");
+  const video = await Video.findById(id).populate("owner").populate("comments");
+  console.log(video);
   if (!video) {
     return res.status(404).render("404", { pageTitle: "Video not found!" });
   }
@@ -116,7 +118,21 @@ export const registerView = async (req, res) => {
   return res.sendStatus(200);
 };
 export const createComment = async (req, res) => {
-  console.log("req.params:", req.params);
-  console.log("req.body:", req.body.text, req.body.rating);
-  res.end();
+  const {
+    session: { user },
+    body: { text },
+    params: { id },
+  } = req;
+  const video = await Video.findById(id);
+  if (!video) {
+    return res.sendStatus(404);
+  }
+  const comment = await Comment.create({
+    text,
+    owner: user._id,
+    video: id,
+  });
+  video.comments.push(comment._id);
+  video.save();
+  return res.sendStatus(201);
 };
